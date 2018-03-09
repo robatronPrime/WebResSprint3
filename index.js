7417b9181c719a3424c041410673de48f95e3360
Robert Stow <UP742010@uni.ds.port.ac.uk>
2018-03-09 10:29:52 +0000

    work done at uni

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-var map;
+let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 2,
-    center: {lat: -33.865427, lng: 151.196123},
+    zoom: 14,
+    center: {lat: 50.79291516421972, lng: -1.1130523681640625},
     mapTypeId: 'terrain'
   });
 
   // Create a <script> tag and set the USGS URL as the source.
-  var script = document.createElement('script');
+  const script = document.createElement('script');
 
   // This example uses a local copy of the GeoJSON stored at
   // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-  script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+  script.src = 'tidePredic.json';
   document.getElementsByTagName('head')[0].appendChild(script);
 
 }
 
 function eqfeed_callback(results) {
-  var heatmapData = [];
-  for (var i = 0; i < results.features.length; i++) {
-    var coords = results.features[i].geometry.coordinates;
-    var latLng = new google.maps.LatLng(coords[1], coords[0]);
-    heatmapData.push(latLng);
+  const heatmapData = [];
+  const portPosData = [];
+  for (let i = 0; i < results.items.length; i++) {
+    let lat = parseFloat(results.items[i].lat);
+    let lng = parseFloat(results.items[i].long);
+    let latLng = new google.maps.LatLng(lat, lng);
+    portPosData.push(latLng);
   }
-  var heatmap = new google.maps.visualization.HeatmapLayer({
+  const heatmap = new google.maps.visualization.HeatmapLayer({
     data: heatmapData,
     dissipating: false,
     map: map
   });
+  const marker = new google.maps.Marker({
+    position: portPosData,
+    map: map
+  })
 }
