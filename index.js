2e6d71156831e16e16f52e1d43edf802243aeb2e
Robert Stow <UP742010@uni.ds.port.ac.uk>
2018-03-23 12:19:41 +0000

    Set up add and remove buttons on click

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 let map;
+let markers = [];
+let portData = [];
+portData = {
+  portPos: [],
+  portName: []
+};
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 5,
@@ -18,15 +25,6 @@ function initMap() {
 }
 
 function callback(results) {
-
-  let markers = [];
-  let portData = [];
-  portData = {
-    portPos: [],
-    portName: []
-  };
-
-
   results.items.forEach(item =>{
     let lat = parseFloat(item.lat);
     let lng = parseFloat(item.long);
@@ -55,69 +53,34 @@ function callback(results) {
       keel:keelClear,
       depth:depth
     });
+    return portData;
+  })
+}
+
+// Sets up markers
+function addMarker(location, infowindow){
+  let marker = new google.maps.Marker({
+    position: location,
+    map: map
   })
 
-  // Sets up markers
-  function addMarker(location, infowindow){
-    let marker = new google.maps.Marker({
-      position: location,
-      map: map
-    })
+  // When a marker is clicked zoom in on that marker
+  // and open a info window
+  marker.addListener('click', function() {
+    map.setZoom(10);
+    map.setCenter(marker.getPosition());
+    infowindow.open(map, marker);
+  })
+  markers.push(marker);
+}
 
-    // When a marker is clicked zoom in on that marker
-    // and open a info window
-    marker.addListener('click', function() {
-      map.setZoom(10);
-      map.setCenter(marker.getPosition());
-      infowindow.open(map, marker);
-    })
-
-    // Push the marker to the markers array
-    function shipCalc(draught) {
-      console.log('foo');
-      clearMarkers();
-      let newPorts = [];
-      const dock = portData.portPos.filter((port) => { if((port.depth -port.keel) < draught) {newPorts.push(port)} })
-      newPorts.forEach(port => {
-        // Create an info window for each port
-        let infowindow = new google.maps.InfoWindow({
-          content: port.name
-        })
-
-        // Add a marker for each port
-        addMarker({lat:port.lat,lng:port.lng}, infowindow)
-      })
-    }
-    markers.push(marker)
-  }
-
-  window.onload=function buttonInit() {
-    const scimitar = document.getElementById("scimitar");
-    const sabre = document.getElementById("sabre");
-    const tyne = document.getElementById("tyne");
-    const quorn = document.getElementById("quorn");
-    const ledbury = document.getElementById("ledbury");
-    const chidd = document.getElementById("chidd");
-    const catt = document.getElementById("catt");
-    const defend = document.getElementById("defend");
-    const kent = document.getElementById("kent");
-    const west = document.getElementById("west");
-    const wave = document.getElementById("wave");
-
-    scimitar.addEventListener('click', shipCalc(1.2));
-    sabre.addEventListener('click', shipCalc(3.3));
-    tyne.addEventListener('click', shipCalc(3.8));
-    quorn.addEventListener('click', shipCalc(2.2));
-    ledbury.addEventListener('click', shipCalc(2.2));
-    chidd.addEventListener('click', shipCalc(2.2));
-    catt.addEventListener('click', shipCalc(2.2));
-    defend.addEventListener('click', shipCalc(7.4));
-    kent.addEventListener('click', shipCalc(6.2));
-    west.addEventListener('click', shipCalc(7.3));
-    wave.addEventListener('click', shipCalc(9.97));
-  }
-
-  portData.portPos.forEach(port => {
+/*
+function shipCalc(draught) {
+  console.log('foo');
+  clearMarkers();
+  let newPorts = [];
+  const dock = portData.portPos.filter((port) => { if((port.depth -port.keel) < draught) {newPorts.push(port)} })
+  newPorts.forEach(port => {
     // Create an info window for each port
     let infowindow = new google.maps.InfoWindow({
       content: port.name
@@ -128,13 +91,27 @@ function callback(results) {
   })
 }
 
-function setMapOnAll(map) {
-  markers.forEach(marker => {
-    marker.setMap(map);
-  })
-}
+  const scimitar = document.getElementById("scimitar");
+  const sabre = document.getElementById("sabre");
+  const tyne = document.getElementById("tyne");
+  const quorn = document.getElementById("quorn");
+  const ledbury = document.getElementById("ledbury");
+  const chidd = document.getElementById("chidd");
+  const catt = document.getElementById("catt");
+  const defend = document.getElementById("defend");
+  const kent = document.getElementById("kent");
+  const west = document.getElementById("west");
+  const wave = document.getElementById("wave");
 
-function clearMarkers() {
-  console.log('bar');
-  setMapOnAll(null);
-}
+  scimitar.addEventListener('click', shipCalc(1.2));
+  sabre.addEventListener('click', shipCalc(3.3));
+  tyne.addEventListener('click', shipCalc(3.8));
+  quorn.addEventListener('click', shipCalc(2.2));
+  ledbury.addEventListener('click', shipCalc(2.2));
+  chidd.addEventListener('click', shipCalc(2.2));
+  catt.addEventListener('click', shipCalc(2.2));
+  defend.addEventListener('click', shipCalc(7.4));
+  kent.addEventListener('click', shipCalc(6.2));
+  west.addEventListener('click', shipCalc(7.3));
+  wave.addEventListener('click', shipCalc(9.97));
+  */
